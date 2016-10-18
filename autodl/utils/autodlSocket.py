83f8a380d62a2e393aaed8e3b434351032c82be0
DjLegolas <djlegolas@users.noreply.github.com>
2016-10-18 17:51:44 +0300

    update autodl socket handler

--- autodl/utils/autodlSocket.py
+++ autodl/utils/autodlSocket.py
@@ -13,10 +13,11 @@ class AutdlSocket:
     _host = "127.0.0.1"
     _password = ""
 
-    def __init__(self, port, password):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.SOL_TCP)
+    def __init__(self, port, password, host=None):
         if port <= 0 or port > 65535:
             raise Exception("Invalid port {}! Initialize port in conf.php".format(port))
+        if host is not None:
+            self._host = host
         self._port = port
         self._password = password
 
@@ -30,12 +31,19 @@ class AutdlSocket:
             data = bytes.join(data, val)
         return data
 
-    def send_autodl_command(self, data):
+    def connect(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.SOL_TCP)
         self._socket.connect((self._host, self._port))
+
+    def disconnect(self):
+        self._socket.close()
+
+    def send(self, data):
         data["password"] = self._password
         self._socket.sendall(json.JSONEncoder().encode(data))
+
+    def recv(self):
         response = self._read_socket(self._socket)
-        self._socket.close()
         json_response = json.JSONDecoder().decode(response)
         return json_response
 
